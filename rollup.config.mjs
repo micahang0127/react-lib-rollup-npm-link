dad9f29c256125793beedb4abc2369718373db7b
PARK SANGA <sanga.park@epikar.com>
2024-01-23 17:08:30 +0900

    init

---
+++ rollup.config.mjs
@@ -0,0 +1,20 @@
+import babel from "@rollup/plugin-babel";
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: './dist/bundle.js',
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    // 바벨 트랜스파일러 설정
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
+
+
